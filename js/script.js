20fbc6295fe0cc6ddd17a9dc34e5756f54fddfd0
Aleksandr Opryshkin <flaming541@gmail.com>
2019-10-15 00:28:53 +0300

    right site bar

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// прилипание правого бара
+var h_hght = 740; // высота бара
+var h_mrg = 0;    // отступ когда бар уже не виден
+
+$(function () {
+
+    var elem = $('.sidebar');
+    var top = $(this).scrollTop();
+
+    if (top > h_hght) {
+        elem.css('top', h_mrg);
+    }
+
+    $(window).scroll(function () {
+        top = $(this).scrollTop();
+
+        if (top + h_mrg < h_hght) {
+            elem.css('top', (h_hght - top));
+        } else {
+            elem.css('top', h_mrg);
+        }
+    });
+
+});
